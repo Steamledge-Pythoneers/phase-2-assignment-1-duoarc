2038b316b0abb569fc683c1ccf56fb3cfd7d5c6c
Abdulrahman Mohammed <abdmuhammad610@gmail.com>
2020-09-28 14:26:18 +0100

    fix: Modify function to handle negative numbers. All tests passed

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -19,14 +19,17 @@ def lowest_terms(x):
     if n2 == 0:
     	return "Undefined"
     elif n2 > 0:
-    	range_stop = n2+1
+        range_stop = n2+1
+        # Iterate through the range of the lowest number in reverse
+        for i in reversed(range(range_stop)):
+            # Check that for factor of both numbers 
+            if n1%i == 0 and n2%i == 0:
+                # Return the result of dividing the numbers by the factor     
+                return "{}/{}".format(n1//i,n2//i)
     else:
-    	range_stop = abs(n2)+1
-
-    
-	# Iterate through the range of the lowest number in reverse 
-    for i in reversed(range(range_stop)):
-		# Check that for factor of both numbers 
-    	if n1%i == 0 and n2%i == 0:
-			# Return the result of dividing the numbers by the factor 
-    		return "{}/{}".format(n1//i,n2//i)
+        range_stop = n2
+        for i in range(range_stop, 0):
+            # Check that for factor of both numbers
+            if n1%i == 0 and n2%i == 0:
+                # Return the result of dividing the numbers by the factor
+                return "{}/{}".format(n1//i,n2//i)
