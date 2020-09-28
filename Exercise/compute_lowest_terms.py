c4aab6504f1e5b8213c9803cd37e9edba324235e
Abdulrahman Mohammed <abdmuhammad610@gmail.com>
2020-09-28 12:18:16 +0100

    bugfix: Handle Zeros in both Numerator and Denominator. 6 tests passed

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -13,13 +13,20 @@ def lowest_terms(x):
     n1 = int(y[0])
     n2 = int(y[1])
     # Set the lower number to be used as range number division
-    range_stop = abs(min([n1,n2]))+1
+    #range_stop = abs(min([n1,n2]))+1
+    if n1 == 0:
+        return "0"
+    if n2 == 0:
+    	return "Undefined"
+    elif n2 > 0:
+    	range_stop = n2+1
+    else:
+    	range_stop = abs(n2)+1
+
     
-    # Check if both numbers are not equal to 0
-    if n1 != 0 and n2 != 0:
-        # Iterate through the range of the lowest number in reverse 
-        for i in reversed(range(range_stop)):
-            # Check that for factor of both numbers 
-            if n1%i == 0 and n2%i == 0:
-                # Return the result of dividing the numbers by the factor 
-                return "{}/{}".format(n1//i,n2//i)
+	# Iterate through the range of the lowest number in reverse 
+    for i in reversed(range(range_stop)):
+		# Check that for factor of both numbers 
+    	if n1%i == 0 and n2%i == 0:
+			# Return the result of dividing the numbers by the factor 
+    		return "{}/{}".format(n1//i,n2//i)
